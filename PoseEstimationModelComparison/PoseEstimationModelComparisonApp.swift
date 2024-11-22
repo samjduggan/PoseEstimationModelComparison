8d7aca70a8b700025c49a4f6ce027642bea003ec
Sam Duggan <samduggan@me.com>
2024-11-22 15:50:18 +0000

    Initial Commit

---
+++ PoseEstimationModelComparison/PoseEstimationModelComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseEstimationModelComparisonApp.swift
+//  PoseEstimationModelComparison
+//
+//  Created by Samuel Duggan on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PoseEstimationModelComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoseEstimationModelComparisonTests/PoseEstimationModelComparisonTests.swift
@@ -0,0 +1,17 @@
+//
+//  PoseEstimationModelComparisonTests.swift
+//  PoseEstimationModelComparisonTests
+//
+//  Created by Samuel Duggan on 22/11/2024.
+//
+
+import Testing
+@testable import PoseEstimationModelComparison
+
+struct PoseEstimationModelComparisonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
